2cd28356d4fdd0a6fa634131086b0db1639dd967
BrunoWrier <brunowrier@gmail.com>
2025-05-14 20:23:58 -0300

    smooth height curve (animation curve) on HeightMapSettings

--- Assets/DerekAssets/Data/HeightMapSettings.cs
+++ Assets/DerekAssets/Data/HeightMapSettings.cs
@@ -1,45 +1,74 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu()]
-public class HeightMapSettings : UpdatableData {
-
-	public NoiseSettings noiseSettings;
-	public NoiseSettings noiseSettingsForHeat;
-	public NoiseSettings noiseSettingsForMoisture;
+public class HeightMapSettings : UpdatableData
+{
+    public NoiseSettings noiseSettings;
+    public NoiseSettings noiseSettingsForHeat;
+    public NoiseSettings noiseSettingsForMoisture;
     public NoiseSettings noiseSettingsForUnits;
 
     public bool useFalloff;
 
-	public float heightMultiplier;
-	public AnimationCurve heightCurve;
+    public float heightMultiplier;
+    public AnimationCurve heightCurve;
 
-	public float minHeight {
-		get {
-			return heightMultiplier * heightCurve.Evaluate (0);
-		}
-	}
+    // [Range(2, 50)]
+    private int smoothResolutionPerSegment = 50;
+	private int oldSmoothResolutionPerSegment = 0;
 
-	public float maxHeight {
-		get {
-			return heightMultiplier * heightCurve.Evaluate (1);
-		}
-	}
+    public float minHeight => heightMultiplier * heightCurve.Evaluate(0);
+    public float maxHeight => heightMultiplier * heightCurve.Evaluate(1);
 
     public float minHeat = 0;
-
     public float maxHeat = 1;
 
     public float minMoisture = 0;
-
     public float maxMoisture = 1;
 
 #if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        noiseSettings.ValidateValues();
+        base.OnValidate();
 
-    protected override void OnValidate() {
-		noiseSettings.ValidateValues ();
-		base.OnValidate ();
-	}
-	#endif
+		if (smoothResolutionPerSegment != oldSmoothResolutionPerSegment){
+				if (smoothResolutionPerSegment > 50) smoothResolutionPerSegment = 50;
+				if (smoothResolutionPerSegment < 1) smoothResolutionPerSegment = 1;
+				oldSmoothResolutionPerSegment = smoothResolutionPerSegment;
+				SmoothHeightCurve();
+		}
+    }
+#endif
 
+    private void SmoothHeightCurve()
+    {
+        if (heightCurve == null || heightCurve.length < 2)
+            return;
+
+        List<Keyframe> smoothKeys = new List<Keyframe>();
+
+        for (int i = 0; i < heightCurve.length - 1; i++)
+        {
+            Keyframe a = heightCurve[i];
+            Keyframe b = heightCurve[i + 1];
+
+            float startTime = a.time;
+            float endTime = b.time;
+            float startValue = a.value;
+            float endValue = b.value;
+
+            for (int j = 0; j <= smoothResolutionPerSegment; j++)
+            {
+                float t = j / (float)smoothResolutionPerSegment;
+                float time = Mathf.Lerp(startTime, endTime, t);
+                float value = Mathf.SmoothStep(startValue, endValue, t);
+                smoothKeys.Add(new Keyframe(time, value));
+            }
+        }
+
+        heightCurve = new AnimationCurve(smoothKeys.ToArray());
+		
+    }
 }
