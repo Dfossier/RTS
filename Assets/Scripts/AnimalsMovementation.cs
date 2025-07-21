06b31e8799b1166ecdc048e62aedf32aa35f43db
BrunoWrier <brunowrier@gmail.com>
2025-07-21 18:28:25 -0300

    animals script conflict bug fix

--- Assets/Scripts/AnimalsMovementation.cs
+++ Assets/Scripts/AnimalsMovementation.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using RTSEngine;
+using RTSEngine.Entities;
+using RTSEngine.EntityComponent;
+using RTSEngine.Movement;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,62 +16,52 @@ public class AnimalsMovementation : MonoBehaviour
     public float waitTime = 3f;
 
     private float timer = 0f;
-    private bool isWaiting = false;
 
     public GameObject animalmodel;
 
     public float timer_tolerance = 0f;
 
+    private UnitMovement unitMovement;
+    private Unit animalEntity;
+    private MovementManager movementManager;
+
     void Start()
     {
-        SetNewDestination();
+        unitMovement = transform.Find("unit_movement").gameObject.GetComponent<UnitMovement>();
+        animalEntity = gameObject.GetComponent<Unit>();
+        movementManager = FindObjectOfType<MovementManager>();
 
+        RandomPosRTSIntegration();
+    }
+
+    void RandomPosRTSIntegration()
+    {
+        Vector3 targetPos;
+
+        if (movementManager.GetRandomMovablePosition(animalEntity, animalEntity.transform.position, wanderRadius, out targetPos, playerCommand: false))
+        {
+            var moveData = new SetPathDestinationData<IEntity>
+            {
+                source = animalEntity,
+                destination = targetPos,
+                offsetRadius = 0f, // 0 means exact destination
+                target = null,
+                mvtSource = new MovementSource { playerCommand = false }
+            };
+
+            movementManager.SetPathDestinationLocal(moveData);
+        }
     }
 
     void Update()
     {
-        if (animator.GetInteger("states") == 2) return;
-        if (agent.isOnNavMesh == false) return;
-        FaceTarget();
-        // Check if agent has reached destination
-        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        
+        if (timer > 8)
         {
-            if (!isWaiting)
-            {
-                isWaiting = true;
-                timer = 0f;
-                agent.isStopped = true;
-                // animator.speed = 0f; // stop animation
-                animator.SetInteger("states", 0);
-            }
-
-            // Wait before moving again
-            timer += Time.deltaTime;
-            if (timer >= waitTime)
-            {
-                isWaiting = false;
-                SetNewDestination();
-            }
+            RandomPosRTSIntegration();
+            timer = 0;
         }
-        else
-        {
-            // Keep moving
-            agent.isStopped = false;
-            // animator.speed = 1f;
-            animator.SetInteger("states", 1);
-
-            // check if it's trying to go to a place that is not really reachable, if it is stucked then change destination after 6 seconds
-            if (agent.velocity.magnitude < 0.5f)
-            {
-                timer_tolerance += Time.deltaTime;
-                if (timer_tolerance >= 6f)
-                {
-                    isWaiting = false;
-                    SetNewDestination(true);
-                    timer_tolerance = 0f;
-                }
-            }
-        }
+        timer += Time.deltaTime;
     }
 
     public void playDeathAnimation()
@@ -78,32 +71,4 @@ public class AnimalsMovementation : MonoBehaviour
         Debug.Log("state changed");
         Debug.Log(animator.GetInteger("states"));
     }
-
-    void SetNewDestination(bool overwrite = false)
-    {
-        if (animator.GetInteger("states") == 2 && overwrite == false) return;
-        agent.speed = 1;
-        agent.angularSpeed = 1;
-        agent.updateRotation = true;
-        Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
-        randomDirection += transform.position;
-
-        if (NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, wanderRadius, NavMesh.AllAreas))
-        {
-            if (!agent.isOnNavMesh) return;
-            agent.SetDestination(hit.position);
-        }
-    }
-    
-    void FaceTarget()
-    {
-        Vector3 direction = agent.velocity;
-        direction.y = 0; // Ignore vertical component for rotation
-        if (direction.sqrMagnitude > 0.01f) // Only rotate if there's significant movement
-        {
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-            animalmodel.transform.rotation = Quaternion.Slerp(animalmodel.transform.rotation, lookRotation, Time.deltaTime * 5f);
-        }
-    }
-
 }
